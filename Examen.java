a5b5b7fd8498cb356ca1dade859cd5e0a110a711
dominmd-ucam <dmartinez37@alu.ucam.edu>
2024-03-17 21:49:47 +0100

    Subo el examen sin refactorizar

---
+++ Examen.java
@@ -0,0 +1,17 @@
+package examenEntornos;
+
+public class Examen {	
+		    
+	  	public static void main(String[] args) {
+	        CarritoDeCompras carrito = new CarritoDeCompras();
+	        Item item1 = new Item("Camiseta", 20.99);
+	        Item item2 = new Item("Pantalones", 34.99);
+	        carrito.agregarItem(item1);
+	        carrito.agregarItem(item2);
+	        carrito.imprimirDetalle();
+
+			System.out.println("Hola me llamo CARLOS");
+	    }
+	}
+
+
